7cfb0a9f72fa44797044d1136e42bb57f1de5619
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2018-03-25 07:07:43 +0900

    improve error handling in list_nodes to show nodes as much as possible

--- epos_hardware/src/tools/list_nodes.cpp
+++ epos_hardware/src/tools/list_nodes.cpp
@@ -3,7 +3,9 @@
 
 #include <epos_hardware/utils.h>
 
+#include <boost/bind.hpp>
 #include <boost/foreach.hpp>
+#include <boost/function.hpp>
 #include <boost/make_shared.hpp>
 #include <boost/program_options/errors.hpp>
 #include <boost/program_options/options_description.hpp>
@@ -13,6 +15,17 @@
 
 namespace eh = epos_hardware;
 namespace bpo = boost::program_options;
+
+// execute a given function in a try-catch barriar
+template < typename T >
+std::vector< T > barriar(const boost::function< std::vector< T >() > &func) {
+  try {
+    return func();
+  } catch (const eh::EposException &error) {
+    std::cerr << error.what() << std::endl;
+  }
+  return std::vector< T >();
+}
 
 int main(int argc, char *argv[]) {
   bool list_rs232;
@@ -43,61 +56,58 @@ int main(int argc, char *argv[]) {
 
   std::cout << "Listing Nodes:" << std::endl;
 
-  try {
-    const std::vector< std::string > device_names(eh::getDeviceNameList());
-    BOOST_FOREACH (const std::string &device_name, device_names) {
-      std::cout << device_name << std::endl;
+  const std::vector< std::string > device_names(
+      barriar< std::string >(boost::bind(eh::getDeviceNameList)));
+  BOOST_FOREACH (const std::string &device_name, device_names) {
+    std::cout << device_name << std::endl;
 
-      const std::vector< std::string > protocol_stack_names(
-          eh::getProtocolStackNameList(device_name));
-      BOOST_FOREACH (const std::string &protocol_stack_name, protocol_stack_names) {
-        std::cout << "\t" << protocol_stack_name << std::endl;
+    const std::vector< std::string > protocol_stack_names(
+        barriar< std::string >(boost::bind(eh::getProtocolStackNameList, device_name)));
+    BOOST_FOREACH (const std::string &protocol_stack_name, protocol_stack_names) {
+      std::cout << "\t" << protocol_stack_name << std::endl;
 
-        const std::vector< std::string > interface_names(
-            eh::getInterfaceNameList(device_name, protocol_stack_name));
-        BOOST_FOREACH (const std::string &interface_name, interface_names) {
-          std::cout << "\t\t" << interface_name << std::endl;
-          if (!list_rs232 && interface_name == "RS232") {
-            std::cout << "\t\t\tSkipping RS232" << std::endl;
-            continue;
+      const std::vector< std::string > interface_names(barriar< std::string >(
+          boost::bind(eh::getInterfaceNameList, device_name, protocol_stack_name)));
+      BOOST_FOREACH (const std::string &interface_name, interface_names) {
+        std::cout << "\t\t" << interface_name << std::endl;
+        if (!list_rs232 && interface_name == "RS232") {
+          std::cout << "\t\t\tSkipping RS232" << std::endl;
+          continue;
+        }
+
+        const std::vector< std::string > port_names(barriar< std::string >(
+            boost::bind(eh::getPortNameList, device_name, protocol_stack_name, interface_name)));
+        BOOST_FOREACH (const std::string &port_name, port_names) {
+          std::cout << "\t\t\t" << port_name << std::endl;
+
+          const std::vector< unsigned int > baudrates(barriar< unsigned int >(boost::bind(
+              eh::getBaudrateList, device_name, protocol_stack_name, interface_name, port_name)));
+          std::cout << "\t\t\t\tBaudrates:" << std::endl;
+          BOOST_FOREACH (unsigned int baudrate, baudrates) {
+            std::cout << "\t\t\t\t\t" << std::dec << baudrate << std::endl;
           }
 
-          const std::vector< std::string > port_names(
-              eh::getPortNameList(device_name, protocol_stack_name, interface_name));
-          BOOST_FOREACH (const std::string &port_name, port_names) {
-            std::cout << "\t\t\t" << port_name << std::endl;
-
-            const std::vector< unsigned int > baudrates(
-                eh::getBaudrateList(device_name, protocol_stack_name, interface_name, port_name));
-            std::cout << "\t\t\t\tBaudrates:" << std::endl;
-            BOOST_FOREACH (unsigned int baudrate, baudrates) {
-              std::cout << "\t\t\t\t\t" << std::dec << baudrate << std::endl;
-            }
-
-            const std::vector< eh::NodeInfo > nodes(eh::enumerateNodes(
-                eh::DeviceInfo(device_name, protocol_stack_name, interface_name, port_name),
-                max_node_id));
-            std::cout << "\t\t\t\tNodes (up to Node Id " << max_node_id << "):" << std::endl;
-            BOOST_FOREACH (const eh::NodeInfo &node, nodes) {
-              std::cout << "\t\t\t\t\tNode Id: " << std::dec << node.node_id << std::endl;
-              std::cout << "\t\t\t\t\t\tSerial Number: 0x" << std::hex << node.serial_number
-                        << std::endl;
-              std::cout << "\t\t\t\t\t\tHardware Version: 0x" << std::hex << node.hardware_version
-                        << std::endl;
-              std::cout << "\t\t\t\t\t\tSoftware Version: 0x" << std::hex << node.software_version
-                        << std::endl;
-              std::cout << "\t\t\t\t\t\tApplication Number: 0x" << std::hex
-                        << node.application_number << std::endl;
-              std::cout << "\t\t\t\t\t\tApplication Version: 0x" << std::hex
-                        << node.application_version << std::endl;
-            }
+          const std::vector< eh::NodeInfo > nodes(barriar< eh::NodeInfo >(boost::bind(
+              eh::enumerateNodes,
+              eh::DeviceInfo(device_name, protocol_stack_name, interface_name, port_name),
+              max_node_id)));
+          std::cout << "\t\t\t\tNodes (up to Node Id " << max_node_id << "):" << std::endl;
+          BOOST_FOREACH (const eh::NodeInfo &node, nodes) {
+            std::cout << "\t\t\t\t\tNode Id: " << std::dec << node.node_id << std::endl;
+            std::cout << "\t\t\t\t\t\tSerial Number: 0x" << std::hex << node.serial_number
+                      << std::endl;
+            std::cout << "\t\t\t\t\t\tHardware Version: 0x" << std::hex << node.hardware_version
+                      << std::endl;
+            std::cout << "\t\t\t\t\t\tSoftware Version: 0x" << std::hex << node.software_version
+                      << std::endl;
+            std::cout << "\t\t\t\t\t\tApplication Number: 0x" << std::hex << node.application_number
+                      << std::endl;
+            std::cout << "\t\t\t\t\t\tApplication Version: 0x" << std::hex
+                      << node.application_version << std::endl;
           }
         }
       }
     }
-  } catch (const eh::EposException &error) {
-    std::cerr << "Error: " << error.what() << std::endl;
-    return 1;
   }
 
   return 0;
